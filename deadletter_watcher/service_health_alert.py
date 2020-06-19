144cd5c6a9a8f789d015435fb6fe9abbe12c0a98
tpilvelis-gw <59439145+tpilvelis-gw@users.noreply.github.com>
2020-06-19 02:32:15 +0100

    Revert "checkpoint, slack blocks created and sending to slack"
    This reverts commit 8aa462e67efb9022e2294dae02ff083bbba1751c.

--- deadletter_watcher/service_health_alert.py
+++
@@ -1,2 +0,0 @@
-# https://docs.microsoft.com/en-us/azure/azure-monitor/platform/action-groups
-# Webhook Source IP address ranges
--- deadletter_watcher/slack_blocks/welcome.py
+++
@@ -1,8 +0,0 @@
-def create_welcome_slack_block(cluster, service, count):
-    return {
-        "type": "section",
-        "text": {
-            "type": "mrkdwn",
-            "text": f"Hi SRE, Oopsie Whoopsie, UwU, You have Service Bus Dead Letters:\nCluster : {cluster}\nService : {service}\nCount : {count}"
-        },
-    }
