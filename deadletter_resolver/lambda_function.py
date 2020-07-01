b767acbf535f3a4283afcd7c7194da2cd44d5dce
tpilvelis-gw <59439145+tpilvelis-gw@users.noreply.github.com>
2020-07-01 22:05:05 +0100

    install pipfile

--- deadletter_resolver/lambda_function.py
+++ deadletter_resolver/lambda_function.py
@@ -3,21 +3,26 @@ import json
 from urllib.parse import parse_qs
 from typing import Tuple
 
+
 def get_user_id(payload) -> str:
     return payload['user']['id']
+
 
 def get_channel_id(payload) -> str:
     return payload['channel']['id']
 
+
 def get_actioned_block_id(payload) -> str:
     return payload['actions'][0]['block_id']
+
 
 def get_slack_block(block_id, payload):
     for block in payload['message']['blocks']:
         if block_id == block['block_id']:
             return block
 
-def get_selected_option_value(payload) -> Tuple[str,str]:
+
+def get_selected_option_value(payload) -> Tuple[str, str]:
     selected_option_value = payload['actions'][0]['value']
     items = selected_option_value.split("-")
     if items[0] == "value":
@@ -26,8 +31,10 @@ def get_selected_option_value(payload) -> Tuple[str,str]:
         else:
             print("Illegal value in action location")
 
+
 def contact_victoria():
     pass
+
 
 def lambda_handler(event, context):
     print(f"event:{event}, context:{context}")
@@ -41,16 +48,11 @@ def lambda_handler(event, context):
 
     trx_id, action = get_selected_option_value(payload)
 
-
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
     }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    response = {"statusCode": 200, "body": json.dumps(body)}
 
     return response
-
