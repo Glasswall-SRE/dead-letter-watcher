221c42f9832a8d7e78a0eb0241e057622ce31cad
tpilvelis-gw <59439145+tpilvelis-gw@users.noreply.github.com>
2020-07-01 14:29:16 +0100

    removed unnessessary whitespace

--- deadletter_listener/lambda_function.py
+++ deadletter_listener/lambda_function.py
@@ -65,7 +65,6 @@ def lambda_handler(event, context):
     queue = triggered_alert.get_service_bus_queue_name()
     cluster = get_cluster(service_bus_name)
     triggered_time = triggered_alert.get_fired_datetime()
-
     triggered_time_obj = get_datetime(triggered_time)
 
     deadletter_ids = obtain_deadletter_ids(queue, cluster, secrets)
