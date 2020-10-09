d805561af3f42034275861b4dde11abbece4abd5
Njogu Mbau <njogumbau@yahoo.com>
2020-10-09 08:31:56 +0300

    added test for empty log

--- deadletter_listener/tests/deadletter_watcher/test_datadog.py
+++ deadletter_listener/tests/deadletter_watcher/test_datadog.py
@@ -1,6 +1,6 @@
 import pytest
 from unittest.mock import patch
-import deadletter_watcher.datadog as datadog 
+import deadletter_watcher.datadog as datadog
 import datetime
 
 @patch('requests.get')
@@ -76,4 +76,27 @@ def test_datadog_log_query_successful_request(patched_emails, patched_tenant):
 @patch('deadletter_watcher.datadog.__datadog_request_tenant_id')
 @patch('deadletter_watcher.datadog.__datadog_request_emails')
 def test_datadog_log_query_email_details_not_found_in_logs(patched_emails, patched_tenant):
-    assert 1 == 1
+    # Arrange
+    message_id = "12345678-1234-1234-1234-12345678901234"
+    secrets = {
+        "DL-WATCHER": {
+            "DD_API_KEY" : "0121-DO-1"
+        }
+    }
+    now = datetime.datetime.now()
+
+    patched_emails.return_value = {
+        # Empty logs
+        'statusCode': 200,
+        'logs': [],
+    }
+
+    # Act
+    deadletter_details = datadog.datadog_log_query(message_id, now, secrets)
+
+    # Assert
+    assert patched_emails.called is True
+    assert patched_tenant.called is False
+    assert deadletter_details['tenant_name'] == "Cannot Find in DataDog Log"
+    assert deadletter_details['sender_email'] == "Cannot Find in DataDog Log"
+    assert deadletter_details['recipient_email'] == "Cannot Find in DataDog Log"
