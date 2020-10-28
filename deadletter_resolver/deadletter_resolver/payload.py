ab311b9e4806f600208d089e82a0b07192ddc1ef
Njogu Mbau <njogumbau@yahoo.com>
2020-10-28 13:52:51 +0300

    added slack req and acknowledgement to slack action

--- deadletter_resolver/deadletter_resolver/payload.py
+++ deadletter_resolver/deadletter_resolver/payload.py
@@ -21,3 +21,10 @@ class Payload:
         value = self.payload_event['actions'][0]['value']
         # e.g value-msgid=123-option=Replay
         return value.split('=')[2]
+
+    def get_response_url(self) -> str:
+        """
+            gets the slack webhook to acknowledge action item from user
+            # e.g https://hooks.slack.com/actions/TRQU3V52S/1452666646531/fxVSwTYsIqbYBHZsmGRKxmfX
+        """
+        return self.payload_event['response_url']
--- deadletter_resolver/lambda_function.py
+++ deadletter_resolver/lambda_function.py
@@ -2,25 +2,37 @@ import json
 from urllib.parse import parse_qs
 from typing import Tuple
 from deadletter_resolver.payload import Payload
-from victoria_email.replay_deadletters import replay
-from victoria_email.reconstruct_mail import reconstruct
+from slack import WebhookClient
+# from victoria_email.replay_deadletters import replay
+# from victoria_email.reconstruct_mail import reconstruct
 
 
 def lambda_handler(event, context):
     print(f"event:{event}, context:{context}")
     # Get the payload from slack action button
-    payload = Payload(json.loads(parse_qs(event['body'])['payload'][0]))
-    # get selected option value
-    value = payload.get_selected_option_value()
+    if event:
+        payload = Payload(json.loads(parse_qs(event['body'])['payload'][0]))
+        # get selected option value
+        value = payload.get_selected_option_value()
+        # get response url
+        response_url = payload.get_response_url()
 
-    if value == 'Replay':
-        # TODO: invoke replay function from victoria
-        message = 'Invoke victoria replay function'
-    elif value == 'Reconstruct':
-        # TODO: invoke replay function from victoria
-        message = 'Invoke Victoria reconstruct function'
+        if value == 'Replay':
+            # TODO: invoke replay function from victoria
+            message = 'Invoke victoria replay function'
+        elif value == 'Reconstruct':
+            # TODO: invoke replay function from victoria
+            message = 'Invoke Victoria reconstruct function'
+        else:
+            message = 'No action item selected'
+
+        # acknowledge action to user
+        slack_client=WebhookClient(response_url)
+        text = "Thanks for your request, we'll process it shortly."
+        slack_client.send(text=text)
+
     else:
-        message = 'No action item selected'
+        message = 'No data passed'
 
     body = {
         "message": message
